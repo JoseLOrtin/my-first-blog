f6c68d25674c4c4a0806ebfcca62757067a4c680
JoseLOrtin <jose@kerdesign.com>
2015-11-30 12:58:04 +0000

    My Django Girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = (
@@ -102,3 +103,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
